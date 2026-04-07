d620a11f05a200f06008e958199220e02adf01bb
David Ström <david.strom@teliacompany.com>
2026-04-07 14:06:15 +0200

    Refactor API client to use a unified getAll method for data fetching in useGames, useGenres, and usePlatforms hooks

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,28 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "1b4125580a6f480d94be72ae50662888"
-    }
-})
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "1b4125580a6f480d94be72ae50662888",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
