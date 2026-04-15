dba6647adf6f8ff04db69e49dabecaab5953af92
David Ström <david.strom@teliacompany.com>
2026-04-15 16:26:08 +0200

    Add GameScreenshots component and useScreenshots hook; update GameDetailPage to display game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,12 +3,12 @@ import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
-  
 
   if (isLoading) return <Spinner />;
 
@@ -20,7 +20,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
-      
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
