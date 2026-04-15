6e82f44089249d3f233f88da1e89a91fd89e0ab3
David Ström <david.strom@teliacompany.com>
2026-04-15 09:14:35 +0200

    Add routing with react-router-dom and implement Home and GameDetail pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import theme from "./theme";
-import "./index.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routes";
+import theme from "./theme";
 
 const quryClient = new QueryClient();
 
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={quryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+    // <Grid
+    //   templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    //   templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+    // >
+    //   <GridItem area="nav">
+    //     <NavBar />
+    //   </GridItem>
+    //   <Outlet />
+    // </Grid>
+  );
+};
+
+export default Layout;
